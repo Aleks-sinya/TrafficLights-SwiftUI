b91ad378ee0a27acf0457cbc42fbdabee3754c13
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-05-12 19:56:02 +0700

    Initial Commit

---
+++ TrafficLights-SwiftUI/TrafficLights_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLights_SwiftUIApp.swift
+//  TrafficLights-SwiftUI
+//
+//  Created by Алексей Синяговский on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLights_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
